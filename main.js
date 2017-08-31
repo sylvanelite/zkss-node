5d71ba3a3ea4ea27128280ebfd645420432455fb
Stephen Pearson <Stephen@Stephens-iMac.local>
2017-08-31 14:13:25 +0930

    postgres

--- main.js
+++ main.js
@@ -74,3 +74,17 @@ server.get('/stop', function(req, res) {
 	started = false;
 	res.send("Stopped");
 });
+
+var pg = require('pg');
+server.get('/db',function (request, response){
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+	
+});
