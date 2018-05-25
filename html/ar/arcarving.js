9d0f9b0af18368c9b7ed97c7a1cfd8017ec78efc
sylvanelite@hotmail.com <Stephen@Stephens-iMac.local>
2018-05-25 15:21:20 +0930

    save

--- html/ar/arcarving.js
+++ html/ar/arcarving.js
@@ -1,24 +1,6 @@
-/*
-http://answers.opencv.org/question/26596/what-does-projection-matrix-provided-by-the-calibration-represent/
-
-http://nghiaho.com/?p=2124
-
-https://en.wikipedia.org/wiki/Camera_resectioning
-
-https://eprints.soton.ac.uk/258885/1/abdullah-art02.pdf
-
-https://www.youtube.com/watch?v=9hAadMszs5k
-
-*/
-
-
-
 var AR = {
-    VOXEL_WIDTH:64,
-    VOXEL_HEIGHT:64,
-    VOXEL_DEPTH:64,
     markerRoots:[],
-    voxels:[]
+    saveData:[]
 };
 AR.beginLoad = function (){
     if (window.ARController && ARController.getUserMediaThreeScene) {
@@ -31,79 +13,24 @@ AR.render = function (){
     requestAnimationFrame(AR.render);
 };
 
-AR.toScreenPositionParticle = function(matrixWorld, pos){
-    var camera = AR.scene.camera;
-    var width = AR.renderer.domElement.width, height = AR.renderer.domElement.height;
-    var widthHalf = width / 2, heightHalf = height / 2;
-    pos = pos.applyMatrix4(matrixWorld);
-    pos.project(camera);
-    
-    pos.x = (pos.x * widthHalf) + widthHalf;
-    pos.y = - (pos.y * heightHalf) + heightHalf;
-    pos.z = 0;
-    return pos;
-};
 AR.cast = function () {
     var cnv = AR.controller.canvas;
     var cnvCtx=cnv.getContext("2d");
     var imgData=cnvCtx.getImageData(0,0,cnv.width,cnv.height);
     var data=imgData.data;
-    var size = AR.voxelGroup.geometry.vertices.length;
-    for(var i=0;i<size;i+=1){
-        var visible=true;
-        var vox = AR.voxelGroup.geometry.vertices[i];
-        var copy = vox.clone();
-        var pos = AR.toScreenPositionParticle(AR.voxelGroup.matrixWorld,copy);
-        var idx = (Math.floor(pos.y)*cnv.width)+Math.floor(pos.x);
-        idx = idx*4;
-        var colour = {r:data[idx],g:data[idx+1],b:data[idx+2]};
-        if(colour.r===undefined){//out of bounds vertex
-            visible=false;
-        }
-        if(colour.g>200){//green (backing screen)
-            visible=false;
-        }
-        if(colour.r>200&&colour.g>200&&colour.b>200){//white (marker)
-            visible=false;
-        }
-        if(colour.g<50&&colour.r<50&&colour.b<50){//black (marker)
-            visible=false;
-        }
-        if(!visible){
-            vox.set(-999999,-999999,-999999);
-        }else{
-            AR.voxelGroup.geometry.colors[i].setRGB(colour.r/255,colour.g/255,colour.b/255);
-        }
-    }
-    AR.voxelGroup.geometry.verticesNeedUpdate=true;
-    AR.voxelGroup.geometry.colorsNeedUpdate=true;
+    AR.saveData.push({
+        data:data,
+        matrixWorld:JSON.stringify(AR.markerRoots[0].elements),
+        matrixCamera:JSON.stringify(AR.scene.camera.matrixWorld.elements)
+    });
 };
 
 AR.loadBarcode = function(barcodeNumb){
     var markerRoot = AR.controller.createThreeBarcodeMarker(barcodeNumb, 1);
-    AR.generateVoxels();
-    markerRoot.add(AR.voxelGroup);
     AR.markerRoots.push(markerRoot);
     AR.scene.scene.add(markerRoot);
 };
-AR.generateVoxels = function (){
-    var scale = 0.1;
-    AR.voxels=[];
-    var geometry = new THREE.Geometry();
-    for(var i=0;i<AR.VOXEL_WIDTH;i+=1){
-        for(var j=0;j<AR.VOXEL_HEIGHT;j+=1){
-            for(var k=0;k<AR.VOXEL_DEPTH;k+=1){
-                geometry.vertices.push(
-                    new THREE.Vector3(j*scale-(AR.VOXEL_WIDTH*scale), k*scale-(AR.VOXEL_HEIGHT*scale), i*scale));
-                geometry.colors.push(
-                    new THREE.Color("rgb(255,0,0)"));
-            }
-        }
-    }
-    var material = new THREE.PointCloudMaterial( { vertexColors: THREE.VertexColors,size:0.1 } );
-    AR.voxelGroup = new THREE.PointCloud(geometry,material);
-    AR.scene.scene.add(AR.voxelGroup);
-};
+
 AR.init = function () {
     var getMediaSuccess = function (arScene, arController, arCamera){
         AR.scene = arScene;
@@ -140,7 +67,6 @@ AR.init = function () {
         onVideoSuccess: videoSuccess,
         cameraParam: "./lib/data/camera_para.dat"
     });
-    $("#capture").on("click",AR.capture);
     $("#cast").on("click",function(){AR.cast();});
 };
 $(document).ready(AR.beginLoad);
