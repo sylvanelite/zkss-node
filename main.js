58fe46639d8ccc50d9523b1d0538116191252f6d
Stephen Pearson <stephen.pearson@sra.com.au>
2020-01-13 13:13:50 +0930

    change set to POST

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const INDEX = path.join(__dirname, './index.html');
 const server = express();
 server.get('/', function(req, res) { res.sendFile(INDEX); });
 server.use('/', express.static(path.join(__dirname, '.')));
-server.use(express.text());
+server.use(express.urlencoded());
 
 let requestHandler = server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
@@ -20,10 +20,9 @@ server.post('/db/doc_set',function (request, response){
 	  connectionString: process.env.DATABASE_URL,
 	  ssl: true,
 	});
-	var body = JSON.parse(request.body);
-	var k = body.data_key;
-	var v = body.data_value;
-	var api = body.api;
+	var k = request.body.data_key;
+	var v = request.body.data_value;
+	var api = request.body.api;
 	var responseObj = {
 		success:false,
 		data:{}
