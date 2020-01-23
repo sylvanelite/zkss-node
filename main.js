14a7b1789d162f6f3110d917de076d8d2e656721
Stephen Pearson <stephen.pearson@sra.com.au>
2020-01-24 09:15:22 +0930

    refactor to check params

--- main.js
+++ main.js
@@ -16,52 +16,62 @@ let requestHandler = server.listen(PORT, () => console.log(`Listening on ${ PORT
 const { Client } = require('pg');
 server.post('/db/doc_set',function (request, response){
 	response.header("Access-Control-Allow-Origin", "*");
-	const client = new Client({
-	  connectionString: process.env.DATABASE_URL,
-	  ssl: true,
-	});
-	var k = request.body.data_key;
-	var v = request.body.data_value;
-	var api = request.body.api;
 	var responseObj = {
 		success:false,
 		data:{}
 	};
-	client.connect();
-    client.query(' SELECT key FROM api WHERE (key=$1) AND writeable=true ',[api], function(err, result) {
-		if (err) {
-			console.error(err);
-			client.end();
-			responseObj.success=false;
-			responseObj.data="Error API lookup";
-			response.send(JSON.stringify(responseObj));
-		}else{
-			if(result.rows[0] &&
-			   result.rows[0].key == api.toUpperCase()){
-				client.query('  INSERT INTO documents (name, content) '+
-				' VALUES ($1, $2) ON CONFLICT (name) DO UPDATE '+
-				' SET content=$2 WHERE documents.name=$1',[k,v], function(err, res) {
-				if (err){
-					console.error(err);
-					client.end();
-					responseObj.success=false;
-					responseObj.data="Error Doc store";
-					response.send(JSON.stringify(responseObj));
-				}else{
-					client.end();
-					responseObj.success=true;
-					responseObj.data="success";
-					response.send(JSON.stringify(responseObj));
-				}
-				});
-			}else{
+	if(!request.body||
+	   !request.body.hasOwnProperty("data_key")||
+	   !request.body.hasOwnProperty("data_value")||
+	   !request.body.hasOwnProperty("api")){
+		responseObj.success=false;
+		responseObj.data="Error params";
+		response.send(JSON.stringify(responseObj));
+	}else{
+		var k = request.body.data_key;
+		var v = request.body.data_value;
+		var api = request.body.api;
+		const client = new Client({
+		  connectionString: process.env.DATABASE_URL,
+		  ssl: true,
+		});
+		client.connect();
+		client.query(' SELECT key FROM api WHERE (key=$1) AND writeable=true ',[api], function(err, result) {
+			if (err) {
+				console.error(err);
 				client.end();
 				responseObj.success=false;
-				responseObj.data="Error API Key";
+				responseObj.data="Error API lookup";
 				response.send(JSON.stringify(responseObj));
+			}else{
+				if(result.rows&&
+				   result.rows[0] &&
+				   result.rows[0].key == api.toUpperCase()){
+					client.query('  INSERT INTO documents (name, content) '+
+					' VALUES ($1, $2) ON CONFLICT (name) DO UPDATE '+
+					' SET content=$2 WHERE documents.name=$1',[k,v], function(err, res) {
+						if (err){
+							console.error(err);
+							client.end();
+							responseObj.success=false;
+							responseObj.data="Error Doc store";
+							response.send(JSON.stringify(responseObj));
+						}else{
+							client.end();
+							responseObj.success=true;
+							responseObj.data="success";
+							response.send(JSON.stringify(responseObj));
+						}
+					});
+				}else{
+					client.end();
+					responseObj.success=false;
+					responseObj.data="Error API Key";
+					response.send(JSON.stringify(responseObj));
+				}
 			}
-		}
-	});
+		});
+	}
 });
 server.get('/db/doc_get',function (request, response){
 	response.header("Access-Control-Allow-Origin", "*");
@@ -69,46 +79,54 @@ server.get('/db/doc_get',function (request, response){
 	  connectionString: process.env.DATABASE_URL,
 	  ssl: true,
 	});
-	var k = request.query.data_key;
-	var api = request.query.api;
 	var responseObj = {
 		success:false,
 		data:{}
 	};
-	client.connect();
-    client.query(' SELECT key FROM api WHERE (key=$1) ',[api], function(err, result) {
-		if (err) {
-			console.error(err);
-			client.end();
-			responseObj.success=false;
-			responseObj.data="Error API lookup";
-			response.send(JSON.stringify(responseObj));
-		}else{
-			if(result.rows[0] &&
-			result.rows[0].key == api.toUpperCase()){
-				client.query('  SELECT name, content FROM documents '+
-				' WHERE  name=$1 ',[k], function(err, res) {
-				if (err){
-					console.error(err);
-					client.end();
-					responseObj.success=false;
-					responseObj.data="Error document store";
-					response.send(JSON.stringify(responseObj));
-				}else{
-					client.end();
-					responseObj.success=true;
-					responseObj.data=res.rows[0].content;
-					response.send(JSON.stringify(responseObj));
-				}
-				});
-			}else{
+	if(!request.query||
+	   !request.query.hasOwnProperty("data_key")||
+	   !request.query.hasOwnProperty("api")||){
+		responseObj.success=false;
+		responseObj.data="Error params";
+		response.send(JSON.stringify(responseObj));
+	}else{
+		var k = request.query.data_key;
+		var api = request.query.api;
+		client.connect();
+		client.query(' SELECT key FROM api WHERE (key=$1) ',[api], function(err, result) {
+			if (err) {
+				console.error(err);
 				client.end();
 				responseObj.success=false;
-				responseObj.data="Error API Key";
+				responseObj.data="Error API lookup";
 				response.send(JSON.stringify(responseObj));
+			}else{
+				if(result.rows[0] &&
+				result.rows[0].key == api.toUpperCase()){
+					client.query('  SELECT name, content FROM documents '+
+					' WHERE  name=$1 ',[k], function(err, res) {
+					if (err){
+						console.error(err);
+						client.end();
+						responseObj.success=false;
+						responseObj.data="Error document store";
+						response.send(JSON.stringify(responseObj));
+					}else{
+						client.end();
+						responseObj.success=true;
+						responseObj.data=res.rows[0].content;
+						response.send(JSON.stringify(responseObj));
+					}
+					});
+				}else{
+					client.end();
+					responseObj.success=false;
+					responseObj.data="Error API Key";
+					response.send(JSON.stringify(responseObj));
+				}
 			}
-		}
-	});
+		});
+	}
 });
 
 server.use('/html', express.static('html'));
