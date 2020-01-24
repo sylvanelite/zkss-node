e93b6a98028edc21f61cbe56bd27542088d4293a
Stephen Pearson <stephen.pearson@sra.com.au>
2020-01-24 11:48:10 +0930

    chaing syntax to promises and pools

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
 	connectionString: process.env.DATABASE_URL,
-	ssl: true,
+	ssl: true
 });
 
 
@@ -57,19 +57,20 @@ server.post('/db/doc_set',function (request, response){
 						' VALUES ($1, $2) ON CONFLICT (name) DO UPDATE '+
 						' SET content=$2 WHERE documents.name=$1',[k,v])
 							.then(function(result2){
-							client.release();
-							responseObj.success=true;
-							responseObj.data="success";
-							response.send(JSON.stringify(responseObj));
+								client.release();
+								responseObj.success=true;
+								responseObj.data="success";
+								response.send(JSON.stringify(responseObj));
 							}).catch(function(err){
 								console.error(err);
-								client.release();
+								client2.release();
 								responseObj.success=false;
 								responseObj.data="Error Doc store";
 								response.send(JSON.stringify(responseObj));
 							});
 						}).catch(function(err){
 							console.error(err);
+							client.release();
 							responseObj.success=false;
 							responseObj.data="Error connecting to Doc store";
 							response.send(JSON.stringify(responseObj));
@@ -85,7 +86,7 @@ server.post('/db/doc_set',function (request, response){
 					responseObj.success=false;
 					responseObj.data="Error API lookup";
 					response.send(JSON.stringify(responseObj));
-				}).then(() => client.release());
+				});
 		}).catch(function(err){
 			console.log(err);
 			responseObj.success=false;
@@ -120,23 +121,32 @@ server.get('/db/doc_get',function  (request, response) {
 				pool.connect().then(function(client2){
 					return client2.query('  SELECT name, content FROM documents '+
 										  ' WHERE  name=$1 ',[k])
-					.then(function(result2){
-					  client2.release();
-					  responseObj.success=true;
-					  responseObj.data=result2.rows[0].content;
-					response.send(JSON.stringify(responseObj));
-					}).catch(function(err){
-					  console.error(err);
-					  responseObj.success=false;
-					  responseObj.data="Error document store retreival";
-					response.send(JSON.stringify(responseObj));
-					});
-				}).catch(function(err){
-				console.error(err);
-				responseObj.success=false;
-				responseObj.data="Error document store connect";
-			  response.send(JSON.stringify(responseObj));
-			  });
+							.then(function(result2){
+											 client2.release();
+								
+								  if(result2.rows&&
+									result2.rows.length>0&&
+									result2.rows[0] ){
+											 responseObj.success=true;
+											 responseObj.data=result2.rows[0].content;
+										   response.send(JSON.stringify(responseObj));
+								  }else{
+											 responseObj.success=false;
+											 responseObj.data="no document found";
+										   response.send(JSON.stringify(responseObj));
+								  }
+							}).catch(function(err){
+							  console.error(err);
+							  responseObj.success=false;
+							  responseObj.data="Error document store retreival";
+							response.send(JSON.stringify(responseObj));
+							});
+						}).catch(function(err){
+						console.error(err);
+						responseObj.success=false;
+						responseObj.data="Error document store connect";
+					  response.send(JSON.stringify(responseObj));
+					  });
 			  }else{
 				  responseObj.success=false;
 				  responseObj.data="Error API Key";
