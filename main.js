071cdf5ea79bf7620383e3d8dffd8a07bdbda742
Stephen Pearson <stephen.pearson@sra.com.au>
2020-01-24 11:24:20 +0930

    chaing syntax to promises and pools

--- main.js
+++ main.js
@@ -47,27 +47,34 @@ server.post('/db/doc_set',function (request, response){
 		pool.connect().then(function(client){
 			return client.query(' SELECT key FROM api WHERE (key=$1) AND writeable=true ',[api])
 			.then(function(result){
+					client.release();
 					if(result.rows&&
 					   result.rows.length>0&&
 					   result.rows[0] &&
 					   result.rows[0].key == api.toUpperCase()){
-						client.query('  INSERT INTO documents (name, content) '+
+						pool.connect().then(function(client2){
+						return client2.query('  INSERT INTO documents (name, content) '+
 						' VALUES ($1, $2) ON CONFLICT (name) DO UPDATE '+
 						' SET content=$2 WHERE documents.name=$1',[k,v])
-						.then(function(res) {
+							.then(function(result2){
 							client.release();
 							responseObj.success=true;
 							responseObj.data="success";
 							response.send(JSON.stringify(responseObj));
+							}).catch(function(err){
+								console.error(err);
+								client.release();
+								responseObj.success=false;
+								responseObj.data="Error Doc store";
+								response.send(JSON.stringify(responseObj));
+							});
 						}).catch(function(err){
 							console.error(err);
-							client.release();
 							responseObj.success=false;
-							responseObj.data="Error Doc store";
+							responseObj.data="Error connecting to Doc store";
 							response.send(JSON.stringify(responseObj));
-						}).then(() => client.end());
+						});
 					}else{
-							client.release();
 						responseObj.success=false;
 						responseObj.data="Error API Key";
 						response.send(JSON.stringify(responseObj));
@@ -105,32 +112,38 @@ server.get('/db/doc_get',function  (request, response) {
 		pool.connect().then(function(client){
 			return client.query(' SELECT key FROM api WHERE (key=$1) ',[api])
 			.then(function(result){
+				client.release();
 			  if(result.rows&&
 				 result.rows.length>0&&
 				 result.rows[0] &&
 				  result.rows[0].key == api.toUpperCase()){
-				  client.query('  SELECT name, content FROM documents '+
-				  ' WHERE  name=$1 ',[k])
-				  .then(function(res){
-					  client.release();
+				pool.connect().then(function(client2){
+					return client2.query('  SELECT name, content FROM documents '+
+										  ' WHERE  name=$1 ',[k])
+					.then(function(result2){
+					  client2.release();
 					  responseObj.success=true;
-					  responseObj.data=res.rows[0].content;
+					  responseObj.data=result2.rows[0].content;
 					response.send(JSON.stringify(responseObj));
-				  }).catch(function(err) {
+					}).catch(function(err){
 					  console.error(err);
-					  client.release();
 					  responseObj.success=false;
-					  responseObj.data="Error document store";
+					  responseObj.data="Error document store retreival";
 					response.send(JSON.stringify(responseObj));
-				  });
+					});
+				}).catch(function(err){
+				console.error(err);
+				responseObj.success=false;
+				responseObj.data="Error document store connect";
+			  response.send(JSON.stringify(responseObj));
+			  });
 			  }else{
-				  client.release();
 				  responseObj.success=false;
 				  responseObj.data="Error API Key";
 					response.send(JSON.stringify(responseObj));
 			  }
 		  }).catch(function(err) {
-			  console.log(client);
+			  console.log(err);
 			  client.release();
 			  responseObj.success=false;
 			  responseObj.data="Error API lookup";
