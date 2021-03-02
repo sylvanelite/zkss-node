a7d664b742644e014a42f92bfea7cc1a039cf352
Stephen Pearson <stephen.pearson@sra.com.au>
2021-03-02 16:21:03 +0930

    db pool to client

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const { Client } = require('pg');
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: true
+    rejectUnauthorized: false
   }
 });
 
