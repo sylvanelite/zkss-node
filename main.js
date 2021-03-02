4735dd643538e481034e117c69bfc226db1650e8
Stephen Pearson <stephen.pearson@sra.com.au>
2021-03-02 15:51:39 +0930

    db pool to client

--- main.js
+++ main.js
@@ -12,21 +12,14 @@ server.use(express.urlencoded({extended:true}));
 
 let requestHandler = server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const { Pool } = require('pg');
+const { Client } = require('pg');
 
-const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
-	ssl: {
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
     rejectUnauthorized: false
   }
 });
-
-
-pool.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err);
-  process.exit(-1);
-});
-
 
 server.post('/db/doc_set',function (request, response){
 	response.header("Access-Control-Allow-Origin", "*");
@@ -45,55 +38,47 @@ server.post('/db/doc_set',function (request, response){
 		var k = request.body.data_key;
 		var v = request.body.data_value;
 		var api = request.body.api;
-		pool.connect().then(function(client){
-			return client.query(' SELECT key FROM api WHERE (key=$1) AND writeable=true ',[api])
-			.then(function(result){
-					client.release();
-					if(result.rows&&
-					   result.rows.length>0&&
-					   result.rows[0] &&
-					   result.rows[0].key == api.toUpperCase()){
-						pool.connect().then(function(client2){
-						return client2.query('  INSERT INTO documents (name, content) '+
-						' VALUES ($1, $2) ON CONFLICT (name) DO UPDATE '+
-						' SET content=$2 WHERE documents.name=$1',[k,v])
-							.then(function(result2){
-								client.release();
-								responseObj.success=true;
-								responseObj.data="success";
-								response.send(JSON.stringify(responseObj));
-							}).catch(function(err){
-								console.error(err);
-								client2.release();
-								responseObj.success=false;
-								responseObj.data="Error Doc store";
-								response.send(JSON.stringify(responseObj));
-							});
+		client.connect();
+		client.query(' SELECT key FROM api WHERE (key=$1) AND writeable=true ',[api])
+		.then(function(result){
+				if(result.rows&&
+					 result.rows.length>0&&
+					 result.rows[0] &&
+					 result.rows[0].key == api.toUpperCase()){
+					let client2 = new Client({
+						connectionString: process.env.DATABASE_URL,
+						ssl: {
+							rejectUnauthorized: false
+						}
+					});
+					client2.query('  INSERT INTO documents (name, content) '+
+					' VALUES ($1, $2) ON CONFLICT (name) DO UPDATE '+
+					' SET content=$2 WHERE documents.name=$1',[k,v])
+						.then(function(result2){
+							responseObj.success=true;
+							responseObj.data="success";
+							response.send(JSON.stringify(responseObj));
 						}).catch(function(err){
 							console.error(err);
-							client.release();
 							responseObj.success=false;
-							responseObj.data="Error connecting to Doc store";
+							responseObj.data="Error Doc store";
 							response.send(JSON.stringify(responseObj));
+						}).then(function(){
+							client2.end();
 						});
-					}else{
-						responseObj.success=false;
-						responseObj.data="Error API Key";
-						response.send(JSON.stringify(responseObj));
-					}
-				}).catch(function(err) {
-					console.log(err);
-					client.release();
+				}else{
 					responseObj.success=false;
-					responseObj.data="Error API lookup";
+					responseObj.data="Error API Key";
 					response.send(JSON.stringify(responseObj));
-				});
-		}).catch(function(err){
-			console.log(err);
-			responseObj.success=false;
-			responseObj.data="Error Connecting to DB";
-			response.send(JSON.stringify(responseObj));
-		});
+				}
+			}).catch(function(err) {
+				console.log(err);
+				client.release();
+				responseObj.success=false;
+				responseObj.data="Error API lookup";
+				response.send(JSON.stringify(responseObj));
+			})
+		.then(function(){client.end();});
 	}
 });
 server.get('/db/doc_get',function  (request, response) {
@@ -111,43 +96,42 @@ server.get('/db/doc_get',function  (request, response) {
 	}else{
 		var k = request.query.data_key;
 		var api = request.query.api;
-		pool.connect().then(function(client){
-			return client.query(' SELECT key FROM api WHERE (key=$1) ',[api])
+		client.query(' SELECT key FROM api WHERE (key=$1) ',[api])
 			.then(function(result){
-				client.release();
 			  if(result.rows&&
 				 result.rows.length>0&&
 				 result.rows[0] &&
 				  result.rows[0].key == api.toUpperCase()){
-				pool.connect().then(function(client2){
-					return client2.query('  SELECT name, content FROM documents '+
+					
+					
+					let client2 = new Client({
+						connectionString: process.env.DATABASE_URL,
+						ssl: {
+							rejectUnauthorized: false
+						}
+					});
+					client2.query('  SELECT name, content FROM documents '+
 										  ' WHERE  name=$1 ',[k])
 							.then(function(result2){
-											 client2.release();
-								
-								  if(result2.rows&&
-									result2.rows.length>0&&
-									result2.rows[0] ){
-											 responseObj.success=true;
-											 responseObj.data=result2.rows[0].content;
-										   response.send(JSON.stringify(responseObj));
-								  }else{
-											 responseObj.success=false;
-											 responseObj.data="no document found";
-										   response.send(JSON.stringify(responseObj));
-								  }
-							}).catch(function(err){
-							  console.error(err);
-							  responseObj.success=false;
-							  responseObj.data="Error document store retreival";
-							response.send(JSON.stringify(responseObj));
-							});
+								if(result2.rows&&
+								result2.rows.length>0&&
+								result2.rows[0] ){
+										 responseObj.success=true;
+										 responseObj.data=result2.rows[0].content;
+										 response.send(JSON.stringify(responseObj));
+								}else{
+										 responseObj.success=false;
+										 responseObj.data="no document found";
+										 response.send(JSON.stringify(responseObj));
+								}
 						}).catch(function(err){
-						console.error(err);
-						responseObj.success=false;
-						responseObj.data="Error document store connect";
-					  response.send(JSON.stringify(responseObj));
-					  });
+							console.error(err);
+							responseObj.success=false;
+							responseObj.data="Error document store retreival";
+						response.send(JSON.stringify(responseObj));
+						}).then(function(){
+								client2.end();
+								});
 			  }else{
 				  responseObj.success=false;
 				  responseObj.data="Error API Key";
@@ -155,17 +139,10 @@ server.get('/db/doc_get',function  (request, response) {
 			  }
 		  }).catch(function(err) {
 			  console.log(err);
-			  client.release();
 			  responseObj.success=false;
 			  responseObj.data="Error API lookup";
-			response.send(JSON.stringify(responseObj));
-		  });
-		}).catch(function(err){
-			console.log(err);
-			responseObj.success=false;
-			responseObj.data="Error Connecting to DB";
-			response.send(JSON.stringify(responseObj));
-		});
+			  response.send(JSON.stringify(responseObj));
+		  }).then(function(){ client.end(); });
 	}
 });
 
@@ -175,7 +152,7 @@ server.use('/html/*/node', function(request, response){
 	try{
 		let pth = "./html/zkss-au/node/getmessage.mjs"//TODO: remove this, request.path??
 		import(pth).then(function(js){
-			js.default(request,response,pool);
+			js.default(request,response,client);
 		});
 	}catch(e){
 			response.send("err: "+e);
